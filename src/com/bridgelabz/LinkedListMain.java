adc10e1162faa7127b8e17b032ab00ceac1c6b3d
Annu <annusinghraj1998@gmail.com>
2022-06-21 00:06:35 +0530

    ADDED : Created LinkedList

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
+import java.util.LinkedList;
+
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to LinkedList Program");
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println("Simple Linked list is : " + list);
     }
 
 
