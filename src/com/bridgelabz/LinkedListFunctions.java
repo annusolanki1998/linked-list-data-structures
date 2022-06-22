b1d089dc1d76ec4cef66ab8193bd0da7014636af
Annu <annusinghraj1998@gmail.com>
2022-06-22 19:55:03 +0530

    MODIFIED : Adding elemets of integer type in Linkedlist

--- src/com/bridgelabz/LinkedListFunctions.java
+++ src/com/bridgelabz/LinkedListFunctions.java
@@ -4,13 +4,20 @@ public class LinkedListFunctions {
     Node head;
 
     public void addFirst(int data) {
-        Node node = new Node(data);
-        node.next = head;
-        head = node;
+        Node newNode = new Node(data);
+        if(head == null){
+            head =newNode;
+            return ;
+        }
+        newNode.next = head;
+        head = newNode;
 
     }
-
     public void printList() {
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
         Node currentNode = head;
         while (currentNode != null) {
             System.out.print(currentNode.data + " -> ");
