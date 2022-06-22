5b014c81c8f918ac80602217359d6e6dcb165747
Annu <annusinghraj1998@gmail.com>
2022-06-22 20:47:19 +0530

    ADDED : Appended elements to the list

--- src/com/bridgelabz/LinkedListFunctions.java
+++ src/com/bridgelabz/LinkedListFunctions.java
@@ -5,16 +5,30 @@ public class LinkedListFunctions {
 
     public void addFirst(int data) {
         Node newNode = new Node(data);
-        if(head == null){
-            head =newNode;
-            return ;
+        if (head == null) {
+            head = newNode;
+            return;
         }
         newNode.next = head;
         head = newNode;
 
     }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currentNode = head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+    }
+
     public void printList() {
-        if(head == null){
+        if (head == null) {
             System.out.println("List is empty");
             return;
         }
@@ -24,5 +38,16 @@ public class LinkedListFunctions {
             currentNode = currentNode.next;
         }
         System.out.println("null");
+    }
+
+    public void appendingNode(int before, int data) {
+        Node currentNode = head;
+        while (currentNode.data != before) {
+            currentNode = currentNode.next;
+        }
+        Node node = new Node(data);
+        node.next = currentNode.next;
+        currentNode.next = node;
+
     }
 }
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -14,10 +14,11 @@ public class LinkedListMain {
 
         LinkedListFunctions linkedListFunctions = new LinkedListFunctions();
         System.out.println("Elements added in the list are :");
-        linkedListFunctions.addFirst(70);
-        linkedListFunctions.addFirst(30);
         linkedListFunctions.addFirst(56);
+        linkedListFunctions.appendingNode(56, 30);
+        linkedListFunctions.appendingNode(30, 70);
         linkedListFunctions.printList();
+
     }
 
 
